d7e64df15d5269c347d5ff6d9ff36c6aa08e78fa
kisublee <yigi2021@gmail.com>
2022-02-11 16:13:14 -0500

    Add Post query and controller.

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,6 +1,43 @@
 // DEPENDENCIES
-const express = require("express")
-const snacksRoute = express.Router()
+const express = require("express");
+const snacksRoute = express.Router();
+const { getAllSnacks, getOneSnack, deleteSnack, createSnack} = require("../queries/snacks");
+
+snacksRoute.get('/', async (_, response) => {
+  const allSnacks = await getAllSnacks();
+  
+  allSnacks.length !== 0 
+  ? response.status(200).json(allSnacks)
+  : response.status(404).json({ error: 'all snacks data is empty' });
+});
+
+// Get a specific snack by id
+snacksRoute.get("/:id", async (request, response) => {
+
+  const { id } = request.params;
+  const snack = await getOneSnack(id);
+
+  if (snack.id) {
+    response.status(200).json(snack);
+  } else {
+    response.status(500).json({ error: `${id} does not exist` });
+  }
+
+});
+
+// Post
+snacksRoute.post("/", async (request, response) => {
+  const newSnack = await createSnack(request.body)
+  response.status(200).json(newSnack)
+})
 
 
-module.exports = snacksRoute
+  snacksRoute.delete('/:id', async (request, response) => {
+    const deletedSnack = await deleteSnack(request.params.id);
+
+    deletedSnack.id 
+    ? response.status(200).json(deletedSnack)
+    : response.status(404).json({ error: `couldn't delete snack at id: ${request.params.id}` });
+  });
+
+module.exports = snacksRoute;
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -3,7 +3,7 @@ const db = require("../db/dbConfig.js");
 const getAllSnacks = async () => {
     try {
         const allSnacks = await db.any(`
-            SELECT * FROM snacks;
+            SELECT * FROM snacks
         `);
         return allSnacks;
     } catch(error) {
@@ -11,6 +11,47 @@ const getAllSnacks = async () => {
     };
 };
 
+// Get id
+const getOneSnack = async (id) => {
+    try{
+        const oneSnack = await db.one("SELECT * FROM snacks WHERE id=$1", id)
+        return oneSnack
+    }catch (err) {
+        return err
+    }
+}
+
+// Create a new snack-log
+const createSnack = async (snack) => {
+
+    const {name, fiber, protein, added_sugar, is_healthy, image} = snack
+    
+        try{
+            const newSnack = await db.one("INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",[name, fiber, protein, added_sugar, is_healthy, image])
+            return newSnack
+        }catch(err) {
+            return err
+        }
+    
+    }
+
+const deleteSnack = async (id) => {
+    try {
+        const deletedSnack = await db.one(`
+            DELETE FROM snacks
+            WHERE id=$1
+            RETURNING *
+        `, id);
+        return deletedSnack;
+    } catch (error) {
+        return error;
+    };
+};
+
 module.exports = {
     getAllSnacks,
+    deleteSnack,
+    getOneSnack,
+    createSnack,
+
 };

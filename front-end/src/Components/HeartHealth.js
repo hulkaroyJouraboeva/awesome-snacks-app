4bdd3e4abc8bedd3ccbf08f860a3cee2fd17a8b3
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-13 20:13:34 -0500

    Show page complete, so is HeartHealth component

--- front-end/src/Components/HeartHealth.js
+++ front-end/src/Components/HeartHealth.js
@@ -4,7 +4,11 @@ import heartOutline from "../assets/heart-regular.png";
 function HeartHealth({ snackHealth }) {
   return (
     <>
-      <p>????</p>
+      <p>
+        snackHealth 
+        ? <img src={heartSolid} alt="filled heart indicating the snack is healthy" />
+        : <img src={heartOutline} alt="empty heart indicating the snack is not healthy" />
+      </p>
     </>
   );
 }
---
+++ front-end/src/Pages/Show.js
@@ -0,0 +1,9 @@
+import SnackDetails from '../Components/SnackDetails';
+
+export default function Show() {
+    return (
+        <div className='Show'>
+            <SnackDetails />
+        </div>
+    );
+};

123cdbf6ab9867a65ee2aa958ae72ca43bca91c8
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-13 22:48:33 -0500

    added a cool "npm run watc" script to package.json

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -24,17 +24,15 @@ const getOneSnack = async (id) => {
 
 // Create a new snack-log
 const createSnack = async (snack) => {
-
     const {name, fiber, protein, added_sugar, is_healthy, image} = snack
     
-        try{
-            const newSnack = await db.one("INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",[name, fiber, protein, added_sugar, is_healthy, image])
-            return newSnack
-        }catch(err) {
-            return err
-        }
-    
-    }
+    try{
+        const newSnack = await db.one("INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",[name, fiber, protein, added_sugar, is_healthy, image])
+        return newSnack
+    }catch(err) {
+        return err
+    };    
+};
 
 const deleteSnack = async (id) => {
     try {

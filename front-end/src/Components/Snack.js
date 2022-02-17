1acc094b8b010bf9974753e4d2a83a1a7af33d3c
Hulkaroy Jouraboeva <hulkaroyjouraboeva@pursuit.org>
2022-02-17 16:45:57 -0500

    passed the 1 test in index we were failing before

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -5,12 +5,13 @@ export default function Snack({ snack }) {
   return (
     <article>
       <Link to={`/snacks/${snack.id}`}>
+        {/* <h4>{snack.name}</h4> */}
         <img src={snack.image} alt={snack.name} />
         <div>
           <h4>
+            {snack.name}
             <HeartHealth snackHealth={snack.is_healthy} />
           </h4>
-          {snack.name}
         </div>
       </Link>
     </article>
